f98cfb4ea7be32832c68ca47517b8a44274179c6
ASSSSN <ohhanjin0330@gmail.com>
2020-04-26 23:04:16 +0900

    프로젝트 초기 설정
    프로젝트 초기 설정

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const path = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+require('dotenv').config();
+const helmet = require('helmet');
+const hpp = require('hpp');
+
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('port', process.env.PORT || 8002);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(morgan('combined'));
+    app.use(helmet());  // 헤더 설정 변경, 웹 취약성 보완
+    app.use(hpp());     // http parameter pollution attack 방지
+} else {
+    app.use(morgan('dev'));
+}
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
+app.use(flash());
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트 대기중...');
+});
