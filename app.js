f26897c59ce0d19a7e379427afc9403791afcacb
ASSSSN <ohhanjin0330@gmail.com>
2020-04-28 19:03:20 +0900

    스키마 추가
    스키마 추가
    몽구스 서버 연결

--- app.js
+++ app.js
@@ -7,23 +7,22 @@ const flash = require('connect-flash');
 require('dotenv').config();
 const helmet = require('helmet');
 const hpp = require('hpp');
-
+const connect = require('./schemas');
 
 const app = express();
+connect();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8002);
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(morgan('combined'));
-    app.use(helmet());  // 헤더 설정 변경, 웹 취약성 보완
-    app.use(hpp());     // http parameter pollution attack 방지
-} else {
-    app.use(morgan('dev'));
-}
+process.env.NODE_ENV === 'production' ? (
+    app.use(morgan('combined')),
+    app.use(helmet()),      // 헤더 설정 변경, 웹 취약성 보완
+    app.use(hpp())          // http parameter pollution attack 방지
+ ) : app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
